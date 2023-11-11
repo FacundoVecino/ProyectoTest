e782b4cf3db8a563cf5c4c988ee1f3e2781c8e19
FacundoVecino <facundovecino32@gmail.com>
2023-11-11 19:58:54 -0300

    Agrego Navbar y Footer

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,7 +1,42 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <header>Navbar</header>
-  )
-}
+    <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" href="/">
+            Restaurante
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarRestaurante"
+            aria-controls="navbarRestaurante"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarRestaurante">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className={`nav-link ${({ isActive }) => isActive ? 'active' : ''}`} aria-current="page" to="/">
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={`nav-link ${({ isActive }) => isActive ? 'active' : ''}`} to="/login">
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
